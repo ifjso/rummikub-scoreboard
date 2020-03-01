4c33c0ed1e8bd3a291fadecad442250aab341afb
ifjso <ojsboy@hotmail.com>
2020-03-02 01:42:20 +0900

    base path 추가

--- rummikub-scoreboard-frontend/src/index.js
+++ rummikub-scoreboard-frontend/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/rummikub-scoreboard">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
