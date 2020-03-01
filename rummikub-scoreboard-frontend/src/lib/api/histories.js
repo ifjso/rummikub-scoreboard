4a32b352d863c39a32b28dfb7f89b6b4bf6a37da
ifjso <ojsboy@hotmail.com>
2020-03-01 19:48:13 +0900

    history 기록 api 삭제

--- rummikub-scoreboard-frontend/src/lib/api/histories.js
+++ rummikub-scoreboard-frontend/src/lib/api/histories.js
@@ -5,6 +5,3 @@ export const listHistories = ({ from, to, skip, limit }) => {
   const queryString = qs.stringify({ from, to, skip, limit });
   return client.get(`/api/histories?${queryString}`);
 };
-
-export const writeHistory = ({ owner, value }) =>
-  client.post('/api/histories', { owner, value });
