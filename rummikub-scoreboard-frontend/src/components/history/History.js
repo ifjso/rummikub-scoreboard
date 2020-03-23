4ef5582ccfc81f9326e4be68690dfb5c71bf7400
ifjso <ojsboy@hotmail.com>
2020-03-23 23:01:48 +0900

    history css 수정

--- rummikub-scoreboard-frontend/src/components/history/History.js
+++ rummikub-scoreboard-frontend/src/components/history/History.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import InfiniteScroll from 'react-infinite-scroller';
 import moment from 'moment';
 import Responsive from '../commons/Responsive';
@@ -8,10 +8,31 @@ import { listHistories } from '../../lib/api/histories';
 const HistoryBlock = styled(Responsive)`
   display: flex;
   width: 100vw;
-  padding: 4rem;
+  padding: 4rem 2rem;
   align-content: center;
   justify-content: center;
   flex-wrap: wrap;
+`;
+
+const InfiniteScrollWithBlock = styled(InfiniteScroll)`
+  width: 100vw;
+`;
+
+const HistoryBox = styled.div`
+  margin-bottom: 2vh;
+  padding: 1.5rem;
+  font-size: 1.1em;
+  border-radius: 1rem;
+  background-color: #fcfff5;
+  color: #2c662d;
+  box-shadow: 0 0 0 2px #a3c293 inset, 0 0 0 0 transparent;
+  ${({ value }) =>
+    value < 0 &&
+    css`
+      background-color: #fff6f6;
+      color: #9f3a38;
+      box-shadow: 0 0 0 2px #e0b4b4 inset, 0 0 0 0 transparent;
+    `}
 `;
 
 const History = () => {
@@ -26,19 +47,24 @@ const History = () => {
 
   return (
     <HistoryBlock>
-      <InfiniteScroll
+      <InfiniteScrollWithBlock
         pageStart={0}
         loadMore={loadFunc}
         hasMore={hasNextPage}
         loader={<h4 key={0}>loading...</h4>}
       >
         {histories.map(history => (
-          <div key={history._id} style={{ marginBottom: '5vh' }}>
-            {moment(history.createdAt).format('YYYY-MM-DD HH:mm:ss.SSS')}{' '}
-            {history.name} {history.value}
-          </div>
+          <HistoryBox key={history._id} value={history.value}>
+            <p>
+              {moment(history.createdAt).format(
+                'YYYY년 MM월 DD일 HH시mm분ss초'
+              )}
+            </p>
+            <p>{history.name}</p>
+            <p>{history.value}</p>
+          </HistoryBox>
         ))}
-      </InfiniteScroll>
+      </InfiniteScrollWithBlock>
     </HistoryBlock>
   );
 };
