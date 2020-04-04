cfb29c8a6c19c367edd45a0478df3e702d76ec54
ifjso <ojsboy@hotmail.com>
2020-04-04 16:03:37 +0900

    Shimmer components 로 분리

---
+++ rummikub-scoreboard-frontend/src/components/commons/Shimmer/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import style from './style';
+
+export default styled.h1`
+  ${style}
+`;
---
+++ rummikub-scoreboard-frontend/src/components/commons/Shimmer/style.js
@@ -0,0 +1,36 @@
+import { css } from 'styled-components';
+
+export default css`
+  color: white;
+  ${({ active }) =>
+    active &&
+    css`
+      color: rgba(255, 255, 255, 0.5);
+      background-color: #222;
+      background-image: linear-gradient(
+        to right,
+        rgba(34, 34, 34, 1),
+        rgba(255, 255, 255, 0.5),
+        rgba(34, 34, 34, 1)
+      );
+
+      --shimmer-size: ${({ shimmerSize = 0.3 }) => `${shimmerSize}em`};
+      background-size: var(--shimmer-size) auto;
+      background-repeat: no-repeat;
+      background-clip: text;
+      -webkit-background-clip: text;
+      animation-name: shimmer;
+      animation-duration: 1s;
+      animation-iteration-count: infinite;
+    `}
+
+  @keyframes shimmer {
+    0% {
+      background-position: calc(0% - var(--shimmer-size)) 0;
+    }
+
+    100% {
+      background-position: calc(100% + var(--shimmer-size)) 0;
+    }
+  }
+`;
