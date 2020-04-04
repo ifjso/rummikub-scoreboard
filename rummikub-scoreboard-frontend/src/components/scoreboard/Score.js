e9ee4d0f7ee200126f615f403a067a3a91b89f99
ifjso <ojsboy@hotmail.com>
2020-04-04 15:28:23 +0900

    Shimmer 추가

--- rummikub-scoreboard-frontend/src/components/scoreboard/Score.js
+++ rummikub-scoreboard-frontend/src/components/scoreboard/Score.js
@@ -57,6 +57,35 @@ const Profile = ({ reversed, user: { name, picture } }) => (
   </ProfileBlock>
 );
 
+const Shimmer = styled.h1`
+  color: rgba(255, 255, 255, 0.5);
+  background-color: #222;
+  background-image: linear-gradient(
+    to right,
+    rgba(34, 34, 34, 1),
+    rgba(255, 255, 255, 0.5),
+    rgba(34, 34, 34, 1)
+  );
+
+  --shimmer-size: 0.35em;
+  background-size: var(--shimmer-size) auto;
+  background-repeat: no-repeat;
+  background-clip: text;
+  -webkit-background-clip: text;
+  animation-name: shimmer;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  @keyframes shimmer {
+    0% {
+      background-position: calc(0% - var(--shimmer-size)) 0;
+    }
+
+    100% {
+      background-position: calc(100% + var(--shimmer-size)) 0;
+    }
+  }
+`;
+
 const Score = ({ reversed = false, user, onClick = i => i }) => (
   <ScoreBlock>
     <Button onClick={() => onClick(user, 1)}>
@@ -64,7 +93,7 @@ const Score = ({ reversed = false, user, onClick = i => i }) => (
     </Button>
     <UserWrapper>
       <Profile reversed={reversed} user={user} />
-      <h1>{user.score}</h1>
+      <Shimmer>{user.score}</Shimmer>
     </UserWrapper>
     <Button onClick={() => onClick(user, -1)}>
       <Icon name="minus" size="small" color="grey" />
