0a7d41fe01d4807a1624362c75427765f58b4bea
ifjso <ojsboy@hotmail.com>
2020-04-07 20:37:50 +0900

    histories action, reducer  추가

---
+++ rummikub-scoreboard-frontend/src/modules/histories.js
@@ -0,0 +1,29 @@
+const LOAD = 'histories/LOAD';
+
+export const load = ({ histories, hasNextPage }) => ({
+  type: LOAD,
+  histories,
+  hasNextPage
+});
+
+const initialState = {
+  histories: [],
+  hasNextPage: true,
+  isLoading: true
+};
+
+const histories = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD:
+      return {
+        ...state,
+        histories: state.histories.concat(action.histories),
+        hasNextPage: action.hasNextPage,
+        isLoading: true
+      };
+    default:
+      return state;
+  }
+};
+
+export default histories;
