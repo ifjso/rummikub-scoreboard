63774a2c176d98c40b36723d1587648faf002fd4
ifjso <ojsboy@hotmail.com>
2020-04-13 21:49:46 +0900

    Merge scoreboard reducer

--- rummikub-scoreboard-frontend/src/modules/index.js
+++ rummikub-scoreboard-frontend/src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import histories from './histories';
+import scoreboard from './scoreboard';
 
-const rootReducer = combineReducers({ histories });
+const rootReducer = combineReducers({ histories, scoreboard });
 
 export default rootReducer;
