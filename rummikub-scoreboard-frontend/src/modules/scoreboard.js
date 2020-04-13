49638b433de59805927c9e28bf41508dd8c9a729
ifjso <ojsboy@hotmail.com>
2020-04-13 21:40:02 +0900

    scoreboard reducer 추가

---
+++ rummikub-scoreboard-frontend/src/modules/scoreboard.js
@@ -0,0 +1,46 @@
+import produce from 'immer';
+
+const READ_USERS = 'scoreboard/READ_USERS';
+const CALCULATE = 'scoreboard/CALCULATE';
+const SAVE_START = 'scoreboard/SAVE_START';
+const SAVE_END = 'scoreboard/SAVE_END';
+
+const initialState = {
+  isLoading: true,
+  form: { selectedIndex: 0, value: 0, isInputting: false },
+  scores: [
+    { user: null, isLoading: false },
+    { user: null, isLoading: false }
+  ]
+};
+
+const scoreboard = (state = initialState, action) => {
+  switch (action.type) {
+    case READ_USERS:
+      return produce(baseState => {
+        baseState.isLoading = false;
+        baseState.scores[0].user = { ...action.users[0] };
+        baseState.scores[1].user = { ...action.uesrs[1] };
+      });
+    case CALCULATE:
+      return produce(baseState => {
+        baseState.form.selectedIndex = action.selectedIndex;
+        baseState.form.value = action.value;
+        baseState.form.isInputting = true;
+      });
+    case SAVE_START:
+      return produce(baseState => {
+        baseState.scores[action.selectedIndex].isLoading = true;
+        baseState.form.isInputting = false;
+      });
+    case SAVE_END:
+      return produce(baseState => {
+        baseState.scores[action.selectedIndex].user = action.user;
+        baseState.scores[action.selectedIndex].isLoading = false;
+      });
+    default:
+      return state;
+  }
+};
+
+export default scoreboard;
