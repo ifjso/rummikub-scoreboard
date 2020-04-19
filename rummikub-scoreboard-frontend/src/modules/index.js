7322c4dc7625483e4d6316e05536ddd48c4038fe
ifjso <ojsboy@hotmail.com>
2020-04-19 18:10:50 +0900

    loading reducer를 root reducer에 포함

--- rummikub-scoreboard-frontend/src/modules/index.js
+++ rummikub-scoreboard-frontend/src/modules/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
+import loading from './loading';
 import histories, { historiesSaga } from './histories';
 import scoreboard, { scoreboardSaga } from './scoreboard';
 import memoModal from './memoModal';
 
-const rootReducer = combineReducers({ histories, scoreboard, memoModal });
+const rootReducer = combineReducers({
+  loading,
+  histories,
+  scoreboard,
+  memoModal
+});
 
 export function* rootSaga() {
   yield all([historiesSaga(), scoreboardSaga()]);
