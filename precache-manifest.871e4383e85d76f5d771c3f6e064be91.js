19ed4cb3ffbf2572f53ca50ccf02bf10448a963b
ifjso <ojsboy@hotmail.com>
2020-04-20 02:30:08 +0900

    Updates

---
+++ precache-manifest.871e4383e85d76f5d771c3f6e064be91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6493fe93983898408fb09a1b3f63d126",
+    "url": "/rummikub-scoreboard/index.html"
+  },
+  {
+    "revision": "51fd99f441e14cc2b3dc",
+    "url": "/rummikub-scoreboard/static/css/main.c11caca2.chunk.css"
+  },
+  {
+    "revision": "28999b09af455b04efa6",
+    "url": "/rummikub-scoreboard/static/js/2.dace29fb.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/rummikub-scoreboard/static/js/2.dace29fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51fd99f441e14cc2b3dc",
+    "url": "/rummikub-scoreboard/static/js/main.03d19399.chunk.js"
+  },
+  {
+    "revision": "f634147539c2ea1947ef",
+    "url": "/rummikub-scoreboard/static/js/runtime-main.6d63a623.js"
+  },
+  {
+    "revision": "38825927627472491999d6c11369ad93",
+    "url": "/rummikub-scoreboard/static/media/histories_inactive.38825927.png"
+  },
+  {
+    "revision": "3334f4a6fdadd994cd75086b2d24a88e",
+    "url": "/rummikub-scoreboard/static/media/scoreboard_inactive.3334f4a6.png"
+  }
+]);
